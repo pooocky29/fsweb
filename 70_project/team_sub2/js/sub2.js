2557e0cdbc8f33e6bd5fc5cfd8beb13d486d32ca
pooocky29 <pooocky@naver.com>
2023-09-08 17:49:24 +0900

    09-08

---
+++ 70_project/team_sub2/js/sub2.js
@@ -0,0 +1,23 @@
+
+
+
+// 숫자값 지정
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const carousel = document.querySelector('.Carousel');
+
+let index = 0;
+
+prevButton.addEventListener('click', () => {
+   if (index === 0) return;
+   index -= 1;
+   
+   carousel.style.transform = `translate3d(-${760 * index}px, 0, 0)`;
+});
+
+nextButton.addEventListener('click', () => {
+   if (index === 2) return;
+   index += 1;
+   
+   carousel.style.transform = `translate3d(-${760 * index}px, 0, 0)`;
+});
