d443e65555ee84425e39bf127d2add70aee7a934
pooocky29 <pooocky@naver.com>
2023-09-13 17:22:37 +0900

    09-13

---
+++ 52_js/71_슬라이드/51-1_슬라이더_data.js
@@ -0,0 +1,26 @@
+
+//데이터 정의
+    const imgObj={
+        imgData1: {
+            imgSrc:'/assets/images/pic1.jpg',
+            imgAlt:'마카롱'
+        },
+        imgData2: {
+            imgSrc:'/assets/images/pic2.jpg',
+            imgAlt:'과자'
+        },
+        imgData3: {
+            imgSrc:'/assets/images/pic3.jpg',
+            imgAlt:'필링'
+        },
+        imgData4: {
+            imgSrc:'/assets/images/pic4.jpg',
+            imgAlt:'뚱카롱'
+        }
+    };
+
+
+
+
+    
+
