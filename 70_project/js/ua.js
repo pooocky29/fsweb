04ee5c01490472b51576888250bf10c5503960ea
pooocky29 <pooocky@naver.com>
2023-09-19 17:48:48 +0900

    09-19

---
+++ 70_project/js/ua.js
@@ -0,0 +1,11 @@
+
+
+    
+    const ua =navigator.userAgent.toLowerCase();
+    if (ua.indexof('window')>0){
+        //ua에 윈도우 문자열이 있으면 데스크탑
+        location.href = 'https://pooocky29.github.io/fsweb/70_project/main.html';
+    }else{
+        location.href = 'https://pooocky29.github.io/fsweb/70_project/m/index.htm';
+    }
+
